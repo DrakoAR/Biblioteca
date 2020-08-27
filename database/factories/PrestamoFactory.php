c50231846baecc51fc4fa0b0932b6a649b6741ef
Ricardo <rik00972@gmail.com>
2020-08-27 04:06:38 -0300

    update factory

--- database/factories/PrestamoFactory.php
+++ database/factories/PrestamoFactory.php
@@ -13,10 +13,12 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\Prestamo::class, function (Faker\Generator $faker) {
+    $book=App\Book::pluck('id')->toarray();
+    $user=App\User::pluck('id')->toarray();
 
     return [
-        'book_id'=>rand(10,20),        
-        'user_id'=>rand(10,20),
+        'book_id'=>$faker->randomElement($book),     
+        'user_id'=>$faker->randomElement($user),
         'devolucion'=>$faker->dateTimeBetween('-10 days','+30days')->format('y-m-d'),     
         'registro_inicio'=>$faker->dateTimeBetween('-30 days','- 5 days')->format('y-m-d'),    
     ];
